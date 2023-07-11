aa9f4929bc140deb57e8d0b9cc03500c4083db4a
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-11 16:18:45 +0530

    done with swap

---
+++ GIT_TASK/src/main/java/TASK_1/swap_with.java
@@ -0,0 +1,15 @@
+package TASK_1;
+
+public class swap_with 
+{
+	public static void main(String[] args) 
+	  {
+		int a=10;
+		int b=20;
+		a=a-b;
+		b=a+b;
+		a=b-a;
+		    
+		    System.out.println("the swap of two numbers is " + a + " is " + b );
+	  }
+}
