e63a53e8206629fc4258fbffff8dd4f240207fac
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-12 12:41:46 +0530

    Create heelo_worldff.java

---
+++ GIT_TASK/src/main/java/TASK_1/heelo_worldff.java
@@ -0,0 +1,11 @@
+package TASK_1;
+
+public class heelo_worldff 
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("hi");
+	System.out.println("hello");
+	System.out.println("done sir");
+  }
+}
