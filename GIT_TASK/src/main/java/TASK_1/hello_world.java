36a03bb030f2d688fe7c089fd9304b7413e76f00
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-13 11:39:24 +0530

    d

--- GIT_TASK/src/main/java/TASK_1/hello_world.java
+++ GIT_TASK/src/main/java/TASK_1/hello_world.java
@@ -7,6 +7,8 @@ public class hello_world
 		System.out.println("hello world");
 		System.err.println("hi");
 		System.out.println("bye");
+		System.out.println("where are you");
+		System.out.println("are");
 	}
 	
 }
