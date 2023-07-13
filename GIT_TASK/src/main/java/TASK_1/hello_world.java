2a28878621378a10e5381da6e7ea994d91beba0f
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-13 12:24:28 +0530

    donr sir.

--- GIT_TASK/src/main/java/TASK_1/hello_world.java
+++ GIT_TASK/src/main/java/TASK_1/hello_world.java
@@ -8,6 +8,8 @@ public class hello_world
 		System.err.println("hi");
 		System.out.println("bye");
 		System.out.println("how are you");
+		System.out.println("hi dad");
+		System.out.println("hi mom");
 	}
 }
 
