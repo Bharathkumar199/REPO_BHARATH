f079e5e9eb78541fd1668c89bc63ad086133297b
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-14 11:04:59 +0530

    done.

---
+++ GIT_TASK/src/main/java/TASK_1/find_duplicate_method.java
@@ -0,0 +1,43 @@
+package TASK_1;
+
+public class find_duplicate_method 
+{
+	public static void dupli(int arr[])
+	{
+
+		System.out.println();
+		for(int i=0;i<arr.length;i++)
+		{
+			int count=0;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					count++;
+					for(int k=i-1;k>=0;k--)
+					{
+						if(arr[k]==arr[i])
+						{
+							count--;
+						}
+					}
+				}
+
+			}
+
+
+			if(count>0)
+			{
+				System.out.println(arr[i]);
+			}
+			count=0;
+		}
+
+	}
+
+	public static void main(String[] args)
+	{		
+		int arr[]= {10,20,20,30,30};
+		dupli(arr);
+	}
+}
