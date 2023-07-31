aa2c653c28f17275533be567a3b0ff3fdde6b991
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-31 13:32:58 +0530

    done

--- Selenium_project/src/Basic/Facebook.java
+++ Selenium_project/src/Basic/Facebook.java
@@ -12,37 +12,41 @@ public static void main(String[] args) throws InterruptedException
 	WebDriver driver =new ChromeDriver();
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
-	
 	Thread.sleep(2000);
 	
 	By by_Username=By.xpath("//input[@id='email']");
 	WebElement ele_mail=driver.findElement(by_Username);
 	ele_mail.sendKeys("9581509211");
-	
 	Thread.sleep(2000);
 	
 	By by_password=By.xpath("//input[@id='pass']");
 	WebElement ele_pass=driver.findElement(by_password);
 	ele_pass.sendKeys("vanijoyce");
-	
 	Thread.sleep(2000);
 	
 	By by_login=By.xpath("//button[@name='login']");
 	WebElement ele_button=driver.findElement(by_login);
 	ele_button.click();
+	Thread.sleep(3000);
 	
-	Thread.sleep(2000);
+	By by_profile1=By.xpath("(//a[@href='https://www.facebook.com/bharathkumar.cfcgvjhv'])[1]");
+	WebElement ele_profile=driver.findElement(by_profile1);
+	ele_profile.click();
+	Thread.sleep(3000);
+	
+	By by_about=By.xpath("//a[@href='https://www.facebook.com/bharathkumar.cfcgvjhv/about']");
+	WebElement ele_about=driver.findElement(by_about);
+	ele_about.click();
+	Thread.sleep(3000);
 	
 	By by_profile=By.xpath("//div[@class='x78zum5 x1n2onr6']");
 	WebElement ele_Icon=driver.findElement(by_profile);
 	ele_Icon.click();
-	
-	Thread.sleep(2000);
+	Thread.sleep(3000);
 	
 	By by_logout=By.xpath("//span[.='Log out']");
 	WebElement ele_Icon1=driver.findElement(by_logout);
 	ele_Icon1.click();
-	
 	Thread.sleep(2000);
 	
 	
---
+++ Selenium_project/src/Basic/W3_schools.java
@@ -0,0 +1,44 @@
+package Basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class W3_schools 
+{
+   public static void main(String[] args) throws InterruptedException 
+   {
+	   WebDriver driver=new ChromeDriver();
+	   driver.get("https://profile.w3schools.com/");
+	   driver.manage().window().maximize();
+	   Thread.sleep(2000);
+	   
+	   By by_UN=By.xpath("//input[@id='modalusername']");
+	   WebElement ele_UN=driver.findElement(by_UN);
+	   ele_UN.sendKeys("bompallibharath@gmail.com");
+	   Thread.sleep(2000);
+	   
+	   By by_Pass=By.xpath("//input[@id='current-password']");
+	   WebElement ele_Pass=driver.findElement(by_Pass);
+	   ele_Pass.sendKeys("Bharath@1419");
+	   Thread.sleep(3000);
+	   
+	   By by_Login=By.xpath("//span[text()='Log in']");
+	   WebElement ele_Login=driver.findElement(by_Login);
+	   ele_Login.click();
+	   Thread.sleep(10000);
+	   
+	  // By by_home=By.xpath("//i[@class='fa fa-logo ws-hover-text-green']");
+	   By by_home = By.xpath("//a[@title='Home']");
+	   Thread.sleep(3000);
+	   WebElement ele_home=driver.findElement(by_home);
+	   Thread.sleep(3000);
+	   
+//	   By by_java=By.xpath("//a[text()='Java Tutorial']");
+//	   WebElement ele_java=driver.findElement(by_java);
+//	   ele_java.click();
+//	   Thread.sleep(2000);
+   }
+}
