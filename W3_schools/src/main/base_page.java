110f22c3d4ee997154f51ff0830e94a9db2a43af
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-08-04 19:06:07 +0530

    dc

--- W3_schools/src/main/base_page.java
+++ W3_schools/src/main/base_page.java
@@ -1,5 +1,17 @@
 package main;
 
-public class base_page {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class base_page 
+{
+   static WebDriver driver;
+   
+   public static void w3()
+   {
+	   driver=new ChromeDriver();
+	   driver.get("https://profile.w3schools.com/");
+	   driver.manage().window().maximize();
+	   
+   }
 }
