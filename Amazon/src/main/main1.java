1edb92275642183341536d3ea344acaedcb71297
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-08-07 14:43:16 +0530

    done>sir

---
+++ Amazon/src/main/main1.java
@@ -0,0 +1,19 @@
+package main;
+
+public class main1 
+{
+   public static void main(String[] args) throws InterruptedException 
+   {
+	   Thread.sleep(2000);
+	  login.amazon();
+	  login.login1();
+	  login.countine();
+	  login.pw();
+	  login.submit();
+	  
+	  Thread.sleep(2000);
+	  title.search();
+	  title.click();
+	  title.mob();
+   }
+}
