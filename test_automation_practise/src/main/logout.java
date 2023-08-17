e73301e9365cf2183390d4d8a9b3023bcba080cd
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-08-17 15:29:09 +0530

    updated

--- test_automation_practise/src/main/logout.java
+++ test_automation_practise/src/main/logout.java
@@ -8,10 +8,13 @@ public class logout extends base_page
 	 static By by_logout=By.xpath("//a[text()='Log out']");
 	 
 	 
-	 static WebElement ele_lougot=driver.findElement(by_logout);
+	 static WebElement lougot()
+	 {
+		 return driver.findElement(by_logout);
+	 }
 	 
 	 public static void lg()
 	 {
-	    ele_lougot.click();
+	    lougot().click();
 	 }
 }
