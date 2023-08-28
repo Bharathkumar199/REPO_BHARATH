271c578a56b045b88bd9c137bd562e134133303f
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-08-28 10:31:38 +0530

    updted

--- Face_book/src/package_utilities/Base_page.java
+++ Face_book/src/package_utilities/Base_page.java
@@ -1,5 +1,7 @@
 package package_utilities;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +13,7 @@ public class Base_page
      driver=new ChromeDriver();
 	 driver.get("https://www.facebook.com/");
 	 driver.manage().window().maximize();
+	 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	}
 
 }
